25bb7399b1c8d4b9f1bca76d0fd83794dbc0164c
Gabriel Freitas Dev <gabrielfreitas.ilustra@gmail.com>
2024-02-22 16:38:17 -0300

    chore(config): fine-tuning site metadata in site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,18 +3,18 @@ import type { AstroExpressiveCodeOptions } from "astro-expressive-code";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "Gabriel Freitas",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "Gabriel Freitas",
 	// Meta property used as the default description meta property
 	description: "An opinionated starter theme for Astro",
 	// HTML lang property, found in src/layouts/Base.astro L:18
-	lang: "en-GB",
+	lang: "pt-BR",
 	// Meta property, found in src/components/BaseHead.astro L:42
-	ogLocale: "en_GB",
+	ogLocale: "pt_BR",
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
-		locale: "en-GB",
+		locale: "pt-BR",
 		options: {
 			day: "numeric",
 			month: "short",
@@ -33,7 +33,7 @@ export const menuLinks: Array<{ title: string; path: string }> = [
 		path: "/",
 	},
 	{
-		title: "About",
+		title: "Sobre mim",
 		path: "/about/",
 	},
 	{
