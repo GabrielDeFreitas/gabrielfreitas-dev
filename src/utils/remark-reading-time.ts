513be008b54c897a8aff5ffa98eaa58bd3ccefd4
Gabriel Freitas Dev <gabrielfreitas.ilustra@gmail.com>
2024-02-26 12:58:17 -0300

    Changes to be committed: 	modified:   src/components/SocialList.astro 	modified:   src/components/layout/Footer.astro 	new file:   src/content/post/draft-post.md 	new file:   src/content/post/long-title.md 	new file:   src/content/post/missing-content.md 	new file:   src/content/post/unique-tags.md 	modified:   src/layouts/BlogPost.astro 	modified:   src/pages/about.astro 	modified:   src/pages/index.astro 	modified:   src/utils/remark-reading-time.ts

--- src/utils/remark-reading-time.ts
+++ src/utils/remark-reading-time.ts
@@ -6,6 +6,6 @@ export function remarkReadingTime() {
 	return function (tree, { data }) {
 		const textOnPage = toString(tree);
 		const readingTime = getReadingTime(textOnPage);
-		data.astro.frontmatter.minutesRead = readingTime.text;
+		data.astro.frontmatter.minutesRead = readingTime.text.replace("read", "de leitura");
 	};
 }
